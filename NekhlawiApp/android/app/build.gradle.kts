05c22cfdc54a5a469b322a58e54f7d548a2eda3c
Donia Alfarhan <doniaabdullah.f@gmail.com>
2026-06-02 18:51:20 +0300

    the version that worked with donia

--- NekhlawiApp/android/app/build.gradle.kts
+++ NekhlawiApp/android/app/build.gradle.kts
@@ -12,17 +12,19 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "1.8"
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.flutter_application_1"
+        multiDexEnabled = true
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 26
@@ -42,4 +44,11 @@ android {
 
 flutter {
     source = "../.."
+}
+dependencies {
+    // Add this line for core library desugaring in KTS
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+
+    // (Optional) If you face multidex issues on older devices, add this too
+    implementation("androidx.multidex:multidex:2.0.1")
 }
